3ea7e44258b4d7b32bd61462fd6fb9a10edc43fe
Matthew Hooker <mwhooker@gmail.com>
2012-12-28 15:30:42 -0800

    explicitely pass variables to template.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,23 +10,23 @@ default[:exhibitor][:transaction_dir] = "/mnt/zookeeper_transactions"
 default[:exhibitor][:log_index_dir] = "/mnt/zookeeper_log_indexes"
 
 # Period (ms) to check for shared config updates.
-default[:exhibitor][:configcheckms] = 30000
+default[:exhibitor][:opts][:optscheckms] = 30000
 # Extra text to display in UI header
-default[:exhibitor][:headingtext] = "Simple"
+default[:exhibitor][:opts][:headingtext] = "Simple"
 # Styling used for the JQuery-based UI. Currently available options: red, black, custom
-default[:exhibitor][:jquerystyle] = "black"
+default[:exhibitor][:opts][:jquerystyle] = "black"
 # Max lines of logging to keep in memory for display.
-default[:exhibitor][:loglines] = "1000"
+default[:exhibitor][:opts][:loglines] = "1000"
 # If true, the Explorer UI will allow nodes to be modified (use with caution).
-default[:exhibitor][:nodemodification] = "false"
+default[:exhibitor][:opts][:nodemodification] = "false"
 # Port for the HTTP Server
-default[:exhibitor][:port] = "8080"
+default[:exhibitor][:opts][:port] = "8080"
 # true/false (default is false).
 # If enabled, ZooKeeper will be queried once a minute for its state via the 'mntr' four letter word
 # (this requires ZooKeeper 3.4.x+). Servo will be used to publish this data via JMX.
-default[:exhibitor][:servo] = "true"
+default[:exhibitor][:opts][:servo] = "true"
 # Connection timeout (ms) for ZK connections.
-default[:exhibitor][:timeout] = "30000"
+default[:exhibitor][:opts][:timeout] = "30000"
 
 
 default[:exhibitor][:environment] = "stage"
