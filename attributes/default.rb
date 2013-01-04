22602711502d08c709faa985d81554aedffd85f0
Matthew Hooker <mwhooker@gmail.com>
2013-01-04 00:28:04 -0800

    resolve #4 and add defaultconfig

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,14 @@ default[:zookeeper][:mirror] = "http://mirrors.ibiblio.org/apache/zookeeper/zook
 default[:gradle][:version] = "1.3"
 default[:gradle][:mirror] = "http://services.gradle.org/distributions/gradle-#{default[:gradle][:version]}-bin.zip"
 
-default[:exhibitor][:version] = "1.4.3"
+default[:exhibitor][:version] = "1.4.4"
 default[:exhibitor][:user] = "zookeeper"
 
 default[:exhibitor][:snapshot_dir] = "/mnt/zookeeper_snapshots"
 default[:exhibitor][:transaction_dir] = "/mnt/zookeeper_transactions"
 default[:exhibitor][:log_index_dir] = "/mnt/zookeeper_log_indexes"
+
+default[:exhibitor][:environment] = "stage"
 
 # If true, enables AWS S3 backup of ZooKeeper log files (s3credentials may be provided as well).
 default[:exhibitor][:opts][:s3backup] = "true"
@@ -32,7 +34,20 @@ default[:exhibitor][:opts][:servo] = "true"
 # Connection timeout (ms) for ZK connections.
 default[:exhibitor][:opts][:timeout] = "30000"
 
+default[:exhibitor][:opts][:defaultconfig] = "#{Chef::Config[:file_cache_path]}/exhibitor-defaultconfig"
+default[:exhibitor][:opts][:s3config] = ""
 
-default[:exhibitor][:environment] = "stage"
-default[:exhibitor][:s3config] = ""
-default[:exhibitor][:s3backup] = "true"
+default[:exhibitor][:defaultconfig][:cleanup_period_ms] = '30000'
+default[:exhibitor][:defaultconfig][:zookeeper_install_directory] = '/opt/zookeeper/*'
+default[:exhibitor][:defaultconfig][:check_ms] = '30000'
+default[:exhibitor][:defaultconfig][:backup_period_ms] = '0'
+default[:exhibitor][:defaultconfig][:client_port] = '2181'
+default[:exhibitor][:defaultconfig][:cleanup_max_files] = '3'
+default[:exhibitor][:defaultconfig][:backup_max_store_ms] = '0'
+default[:exhibitor][:defaultconfig][:connect_port] = '2888'
+default[:exhibitor][:defaultconfig][:backup_extra] = ''
+default[:exhibitor][:defaultconfig][:observer_threshold] = '0'
+default[:exhibitor][:defaultconfig][:election_port] = '3888'
+default[:exhibitor][:defaultconfig][:zoo_cfg_extra] = 'tickTime\=2000&initLimit\=10&syncLimit\=5'
+default[:exhibitor][:defaultconfig][:auto_manage_instances_settling_period_ms] = '0'
+default[:exhibitor][:defaultconfig][:auto_manage_instances] = '1'
