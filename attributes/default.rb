e82140d37c11b6d03a5f75e7d70635b4250668aa
Matthew Hooker <mwhooker@gmail.com>
2013-01-29 01:27:51 -0800

    comment out all config values

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,19 +14,19 @@ default[:exhibitor][:log_index_dir] = "/tmp/zookeeper_log_indexes"
 default[:exhibitor][:environment] = "stage"
 
 # If true, enables AWS S3 backup of ZooKeeper log files (s3credentials may be provided as well).
-default[:exhibitor][:opts][:s3backup] = "false"
+# default[:exhibitor][:opts][:s3backup] = "false"
 # Max lines of logging to keep in memory for display.
-default[:exhibitor][:opts][:loglines] = "1000"
+# default[:exhibitor][:opts][:loglines] = "1000"
 # If true, the Explorer UI will allow nodes to be modified (use with caution).
-default[:exhibitor][:opts][:nodemodification] = "false"
+# default[:exhibitor][:opts][:nodemodification] = "false"
 # Port for the HTTP Server
-default[:exhibitor][:opts][:port] = "8080"
+# default[:exhibitor][:opts][:port] = "8080"
 # true/false (default is false).
 # If enabled, ZooKeeper will be queried once a minute for its state via the 'mntr' four letter word
 # (this requires ZooKeeper 3.4.x+). Servo will be used to publish this data via JMX.
-default[:exhibitor][:opts][:servo] = "false"
+# default[:exhibitor][:opts][:servo] = "false"
 # Connection timeout (ms) for ZK connections.
-default[:exhibitor][:opts][:timeout] = "30000"
+#default[:exhibitor][:opts][:timeout] = "30000"
 # Period (ms) to check for shared config updates.
 #default[:exhibitor][:opts][:configcheckms] = 30000
 # Styling used for the JQuery-based UI. Currently available options: red, black, custom
