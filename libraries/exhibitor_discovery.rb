a4f50bbac04098b6860da3c5351b4225425eb41b
Matthew Hooker <mwhooker@gmail.com>
2013-02-03 17:49:37 -0800

    discovery utility library

---
+++ libraries/exhibitor_discovery.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: exhibitor_discovery
+# Recipe:: default
+#
+# Copyright 2013, Simple Finance Technology Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+require 'json'
+require 'net/http'
+require 'uri'
+
+
+def discover_zookeepers(exhibitor_host)
+    url = URI.parse(exhibitor_host) + '/exhibitor/v1/cluster/list'
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+          http.request(req)
+    }
+    return JSON.parse(res.body)
+end
