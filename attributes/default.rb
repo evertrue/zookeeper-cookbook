0b649b0c8395c06484d3ede20e9475696b11152a
Steven Cary <steve@goindex.com>
2013-02-14 09:25:19 -0800

    Update hostname defaults to work properly.

--- attributes/default.rb
+++ attributes/default.rb
@@ -37,7 +37,11 @@ default[:exhibitor][:log_index_dir] = "/tmp/zookeeper_log_indexes"
 # Extra text to display in UI header
 #default[:exhibitor][:opts][:headingtext] = "Exhibitor"
 
-default[:exhibitor][:opts][:hostname] =  node[:cloud][:public_hostname]
+if node[:cloud]
+  default[:exhibitor][:opts][:hostname] =  node[:cloud][:public_hostname]
+else
+  default[:exhibitor][:opts][:hostname] =  node[:ipaddress]
+end
 default[:exhibitor][:opts][:defaultconfig] = "#{Chef::Config[:file_cache_path]}/exhibitor-defaultconfig"
 
 default[:exhibitor][:opts][:configtype] = "file"
