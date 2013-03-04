5ce207567b81245d50ac903bea75f9630ca9f733
Matthew Hooker <mwhooker@gmail.com>
2013-03-04 12:51:04 -0800

    put require in try/catch

--- providers/zookeeper.rb
+++ providers/zookeeper.rb
@@ -1,6 +1,10 @@
+begin
+  require 'zookeeper'
+rescue LoadError
+  Chef::Log.warn("Missing gem 'zookeeper'")
+end
 
 def get_zk()
-  require 'zookeeper'
   # todo: memoize
   return Zookeeper.new(@new_resource.zk_connection)
 end
