bf1cae536ed923835484ed118d5a02de6f0dd8f1
Matthew Hooker <mwhooker@gmail.com>
2013-03-04 23:09:43 -0800

    default name

--- resources/node.rb
+++ resources/node.rb
@@ -1,7 +1,7 @@
 actions :create, :delete, :create_if_missing
 default_action :create if defined?(default_action)
 
-attribute :path, :kind_of => String, :name_attribute => true
+attribute :path, :kind_of => String, :default => '/', :name_attribute => true
 attribute :connect_str, :kind_of => String, :required => true
 attribute :data, :kind_of => String
 
