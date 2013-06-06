39f0cdaec6d4b9434168c2a1e4fd3e3a4233e38e
Matthew Hooker <mwhooker@gmail.com>
2013-06-06 13:29:03 -0700

    simplify.

--- files/default/check_stat.py
+++ files/default/check_stat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import itertools
 import json
 import socket
 import sys
@@ -11,7 +10,7 @@ import urlparse
 from check_exhibitor import fetch_status
 
 def get_stat(host, hostname):
-    path = '/exhibitor/v1/cluster/4ltr/stat/' + hostname
+    path = '/exhibitor/v1/cluster/4ltr/mntr/' + hostname
 
     stat_url = urlparse.urljoin(host, path)
     f = urllib.urlopen(stat_url)
@@ -29,15 +28,11 @@ def do(host):
 
 
 def parse_response(response):
-    vals = response.split("\\n")
-    for kv in vals:
-        yield kv.split(': ', 1)
+    return json.loads(response).strip("\n").split("\n")
 
 if __name__ == '__main__':
     root_host = sys.argv[1]
     socket.setdefaulttimeout(1)
     for host, resp in do(root_host):
-        for kv in itertools.islice(
-            itertools.ifilter(lambda x: len(x) == 2, resp), 2, 9):
-
-            print "exhibitor.%s.%s\t%s\t%d" % (host, kv[0], kv[1], time.time())
+        for kv in resp:
+            print "exhibitor.%s.%s\t%d" % (host, kv, time.time())
