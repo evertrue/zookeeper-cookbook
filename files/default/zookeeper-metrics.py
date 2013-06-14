3a1e56ad21b5e6b854e5f8e66c5cc7b71412647e
Matthew Hooker <mwhooker@gmail.com>
2013-06-14 14:01:57 -0700

    optional localhost or fanout.

--- files/default/zookeeper-metrics.py
+++ files/default/zookeeper-metrics.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 import socket
-import sys
 import time
 import urllib
 import urlparse
@@ -21,21 +21,38 @@ def get_stat(host, hostname):
     return json.loads(f.read())['response']
 
 
-def do(host):
+def stat_fanout(host):
     hosts = [h['hostname'] for h in fetch_status(host)]
     for hostname in hosts:
         yield hostname, parse_response(get_stat(host, hostname))
 
+def stat_localhost(host):
+    yield socket.gethostname(), parse_response(get_stat(host, 'localhost'))
 
 def parse_response(response):
     return response.decode('string-escape').strip("\n\"").split("\n")
 
-if __name__ == '__main__':
-    path_prefix = sys.argv[1].rstrip('.')
-    root_host = sys.argv[2]
+
+def main(prefix, hostname, stat_generator):
     socket.setdefaulttimeout(1)
-    for host, resp in do(root_host):
+    for host, resp in stat_generator(hostname):
         # kv takes the form {key}\t{value}
         for kv in resp:
             print "%s.exhibitor.%s.%s\t%d" % (
-                path_prefix, host.replace('.', '-'), kv, time.time())
+                prefix, host.replace('.', '-'), kv, time.time())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('prefix', help='graphite key prefix.')
+    parser.add_argument('host', help='host to collect metrics from')
+    parser.add_argument('--fan-out', action='store_true', default=False,
+                        help='If specified, fanout to all zookeepers and return combined metrics.')
+
+    args = parser.parse_args()
+    path_prefix = args.prefix.rstrip('.')
+
+    if args.fan_out:
+        main(path_prefix, args.host, stat_fanout)
+    else:
+        main(path_prefix, args.host, stat_localhost)
