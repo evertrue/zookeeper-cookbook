b32243dfb0b04b794359f1265363e089497d08b1
Jake Davis <jake.davis5989@gmail.com>
2014-06-11 16:32:36 +0000

    Move config helper to new default library and use it

--- providers/config.rb
+++ providers/config.rb
@@ -1,5 +1,7 @@
 # providers/config.rb
 
+include Zookeeper::Config
+
 def initialize(new_resource, run_context)
   super
   @path   = new_resource.path
@@ -11,7 +13,7 @@ end
 action :render do
   @zoocfg.owner(@user)
   @zoocfg.group(@user)
-  @zoocfg.content(render_config(@config))
+  @zoocfg.content(render_zk_config(@config))
   @zoocfg.run_action(:create)
 end
 
@@ -21,26 +23,6 @@ action :delete do
 end
 
 private
-
-# Zookeeper uses a Java properties file style of configuration. This helper
-# method will render a hash in that style.
-def render_config(config, lead=nil)
-  rendered = ""
-
-  config.each_pair do |k,v|
-    if lead
-      rendered << "#{lead}."
-    end
-
-    if v.is_a?(Hash)
-      rendered << render_config(v, k)
-    else
-      rendered << "#{k}=#{v}\n"
-    end
-  end
-
-  return rendered
-end
 
 def zookeeper_config_resource(path='')
   return Chef::Resource::File.new(path, @run_context)
