ea1f36347af00583a6617326802f7f2b427665a3
Scott Lovenberg <scott.lovenberg@gmail.com>
2014-08-22 16:11:30 -0500

    Set minimum build-essentials ver for RHEL
    Versions of build-essentials < 2.0.2 don't seem to work on RHEL-based
    operating systems. Build-essentials-2.0.2 has the required patch merged
    (4d9f91c101588ab5292259fd06c1b50d935b7f73), so make that the minimum.
    Ref: https://github.com/opscode-cookbooks/build-essential/pull/31
    
    Signed-off-by: Scott Lovenberg <scott.lovenberg@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'Apache v2.0'
 description      'Installs/Configures zookeeper'
 version          '2.2.0'
 
-depends          'build-essential'
+depends          'build-essential', ">= 2.0.2"
 depends          'java'
 depends          'runit'
