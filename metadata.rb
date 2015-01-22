0a0856e0fa4082af8e46a58843a9a15adfbe22f3
Jeff Byrnes <jeff@evertrue.com>
2015-01-22 18:14:49 -0500

    Add `supports` metadata
    Fixes #120

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,11 @@ license          'Apache v2.0'
 description      'Installs/Configures zookeeper'
 version          '2.5.1'
 
+supports         'ubuntu', '= 12.04'
+supports         'ubuntu', '= 14.04'
+supports         'centos', '= 6.4'
+supports         'centos', '= 6.5'
+
 depends          'build-essential', '>= 2.0.2'
 depends          'java'
 depends          'runit'
