6902c9f0af99b9720d338984f11398918164115d
Jeff Byrnes <jeff@evertrue.com>
2015-08-13 17:04:32 -0400

    Add additional test to test default attributes

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,28 +2,21 @@
 
 require 'spec_helper'
 
-describe service('zookeeper') do
-  it { should be_running }
-end
-
-describe port(2181) do
-  it { should be_listening }
-end
-
-describe 'zookeeper_node' do
-  it 'should create /testing' do
-    zookeeper = Zookeeper.new 'localhost:2181'
-    data = zookeeper.get path: '/testing'
-    expect(data[:stat].exists?).to eq true
-    expect(data[:data]).to eq 'some data'
+context 'when all attributes are default' do
+  describe service('zookeeper') do
+    it { should be_running }
   end
-end
 
-describe file '/opt/zookeeper/zookeeper-3.4.6/conf/zookeeper-env.sh' do
-  it { should be_file }
-  describe '#content' do
-    subject { super().content }
-    it { should include 'ZOO_LOG4J_PROP=INFO,ROLLINGFILE' }
-    it { should include 'ZOO_LOG_DIR=/var/log/zookeeper' }
+  describe port(2181) do
+    it { should be_listening }
+  end
+
+  describe 'zookeeper_node' do
+    it 'should create /testing' do
+      zookeeper = Zookeeper.new 'localhost:2181'
+      data = zookeeper.get path: '/testing'
+      expect(data[:stat].exists?).to eq true
+      expect(data[:data]).to eq 'some data'
+    end
   end
 end
---
+++ test/integration/zookeeper_env/serverspec/zookeeper_env_spec.rb
@@ -0,0 +1,14 @@
+# coding: UTF-8
+
+require 'spec_helper'
+
+context 'when node[:zookeeper][:env_vars] is set' do
+  describe file '/opt/zookeeper/zookeeper-3.4.6/conf/zookeeper-env.sh' do
+    it { should be_file }
+    describe '#content' do
+      subject { super().content }
+      it { should include 'ZOO_LOG4J_PROP=INFO,ROLLINGFILE' }
+      it { should include 'ZOO_LOG_DIR=/var/log/zookeeper' }
+    end
+  end
+end
