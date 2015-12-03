260c6674cb176a294015b44a694efdf833a3bd58
Anthony Scalisi <scalisi.a@gmail.com>
2015-12-03 11:12:57 -0800

    bump zk version to 3.4.7 (latest stable)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # attributes/default.rb
 
-default[:zookeeper][:version]     = '3.4.6'
-default[:zookeeper][:checksum]    = '01b3938547cd620dc4c93efe07c0360411f4a66962a70500b163b59014046994'
+default[:zookeeper][:version]     = '3.4.7'
+default[:zookeeper][:checksum]    = '2e043e04c4da82fbdb38a68e585f3317535b3842c726e0993312948afcc83870'
 default[:zookeeper][:mirror]      = 'http://www.poolsaboveground.com/apache/zookeeper'
 default[:zookeeper][:user]        = 'zookeeper'
 default[:zookeeper][:install_dir] = '/opt/zookeeper'
