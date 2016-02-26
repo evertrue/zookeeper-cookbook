298fcb59fdc04f07ec1a9c643bc8260af3a555df
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-02-26 16:39:27 -0500

    FC001: Use strings in preference to symbols

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,27 +1,25 @@
 # attributes/default.rb
 
-default[:apt][:compile_time_update] = true
+default['apt']['compile_time_update'] = true
 
-default[:zookeeper][:version]     = '3.4.8'
-default[:zookeeper][:checksum]    =
+default['zookeeper']['version']     = '3.4.8'
+default['zookeeper']['checksum']    =
   'f10a0b51f45c4f64c1fe69ef713abf9eb9571bc7385a82da892e83bb6c965e90'
-default[:zookeeper][:mirror]      = 'http://www.poolsaboveground.com/apache/zookeeper'
-default[:zookeeper][:user]        = 'zookeeper'
-default[:zookeeper][:user_home]   = '/home/zookeeper'
-default[:zookeeper][:install_dir] = '/opt/zookeeper'
-default[:zookeeper][:use_java_cookbook] = true
-default[:zookeeper][:config_dir]  = "#{node[:zookeeper][:install_dir]}/" \
+default['zookeeper']['mirror']      = 'http://www.poolsaboveground.com/apache/zookeeper'
+default['zookeeper']['user']        = 'zookeeper'
+default['zookeeper']['user_home']   = '/home/zookeeper'
+default['zookeeper']['install_dir'] = '/opt/zookeeper'
+default['zookeeper']['use_java_cookbook'] = true
+default['zookeeper']['config_dir']  = "#{node['zookeeper']['install_dir']}/" \
                                     'zookeeper-%{zookeeper_version}/conf'
-default[:zookeeper][:conf_file]   = 'zoo.cfg'
-default[:zookeeper][:java_opts]   = '-Xms128M -Xmx512M'
-default[:zookeeper][:jmx_port]    = '7199'
-default[:zookeeper][:jmx_local]    = 'false'
-default[:zookeeper][:log_dir]     = '/var/log/zookeeper'
+default['zookeeper']['conf_file']   = 'zoo.cfg'
+default['zookeeper']['java_opts']   = '-Xms128M -Xmx512M'
+default['zookeeper']['log_dir']     = '/var/log/zookeeper'
 
 # One of: 'upstart', 'runit', 'exhibitor', 'sysv'
-default[:zookeeper][:service_style] = 'runit'
+default['zookeeper']['service_style'] = 'runit'
 
-default[:zookeeper][:config] = {
+default['zookeeper']['config'] = {
   clientPort: 2181,
   dataDir: '/var/lib/zookeeper',
   tickTime: 2000,
@@ -30,11 +28,11 @@ default[:zookeeper][:config] = {
 }
 
 # Set a default value to avoid Ruby errors
-default[:zookeeper][:env_vars] = false
+default['zookeeper']['env_vars'] = false
 
 # Examples of optional environment vars
 # See the zookeeper config files (conf/zkEnv.sh, etc.) for more examples
-# set[:zookeeper][:env_vars] = {
+# set['zookeeper']['env_vars'] = {
 #   ZOO_LOG4J_PROP: 'INFO,ROLLINGFILE',
 #   ZOO_LOG_DIR: '/var/log/zookeeper'
 # }
