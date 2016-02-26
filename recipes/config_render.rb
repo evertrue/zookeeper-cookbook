fa241eec3272a3ba062b300686d46befeca09015
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-02-26 16:40:26 -0500

    FC002: Avoid string interpolation if unnecessary

--- recipes/config_render.rb
+++ recipes/config_render.rb
@@ -17,8 +17,8 @@
 # set the config path based on default attributes
 # render out our config
 zookeeper_config 'zookeeper config' do
-  path   "#{node['zookeeper']['config_dir'] % { zookeeper_version: node['zookeeper']['version'] }}/" \
-         "#{node['zookeeper']['conf_file']}"
+  path   "#{node['zookeeper']['config_dir'] % { zookeeper_version: node['zookeeper']['version'] }}/" +
+         node['zookeeper']['conf_file']
   config node['zookeeper']['config']
   user   node['zookeeper']['user']
   action :render
