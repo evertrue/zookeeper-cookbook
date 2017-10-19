38efbe8d036a867ba6c030ce32b93cd545a02fbc
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-10-19 11:55:29 -0400

    Set all ChefSpec config in spec_helper
    Avoids extra files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,10 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  config.platform = 'ubuntu'
-  config.version  = '14.04'
+  config.platform  = 'ubuntu'
+  config.version   = '16.04'
+  config.color     = true
+  config.formatter = :documentation
 end
 
 at_exit { ChefSpec::Coverage.report! }
