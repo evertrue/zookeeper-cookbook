385d85ed3d4eae768f9c6564150b8ac2a174920b
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-05-09 18:24:16 -0400

    Upgrade to ZooKeeper v3.4.12
    Fixes #216

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@
 
 allocated_memory = "#{(node['memory']['total'].to_i * 0.8).floor / 1024}m"
 
-default['zookeeper']['version']     = '3.4.11'
+default['zookeeper']['version']     = '3.4.12'
 default['zookeeper']['checksum']    =
-  'f6bd68a1c8f7c13ea4c2c99f13082d0d71ac464ffaf3bf7a365879ab6ad10e84'
+  'c686f9319050565b58e642149cb9e4c9cc8c7207aacc2cb70c5c0672849594b9'
 default['zookeeper']['mirror']      = 'http://apache.mirrors.tds.net/zookeeper/'
 default['zookeeper']['user']        = 'zookeeper'
 default['zookeeper']['user_home']   = '/home/zookeeper'
