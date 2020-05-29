254e48d415668bd7efdb1ac339afdfbe1fea7453
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-05-28 22:38:51 -0400

    test: Add testing on Ubuntu 20.04 & CentOS 8

--- metadata.rb
+++ metadata.rb
@@ -9,9 +9,9 @@ issues_url 'https://github.com/evertrue/zookeeper-cookbook/issues'
 source_url 'https://github.com/evertrue/zookeeper-cookbook/'
 
 supports         'ubuntu', '>= 16.04'
-supports         'centos', '~> 7.0'
-supports         'oracle', '~> 7.0'
-supports         'redhat', '~> 7.0'
+supports         'centos', '>= 7.0'
+supports         'oracle', '>= 7.0'
+supports         'redhat', '>= 7.0'
 
 chef_version     '>= 13.0'
 depends          'java', '>= 1.39'
