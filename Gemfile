c4e9e42121c7032975a214bbbd6eaa8fe25991f4
Jeff Byrnes <thejeffbyrnes@gmail.com>
2022-06-25 15:39:12 -0400

    Update support config files
    Based on the @Sous-Chefs standards, details here:
    https://github.com/sous-chefs/repo-management/

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'community_cookbook_releaser'
--- metadata.rb
+++ metadata.rb
@@ -4,16 +4,15 @@ maintainer_email 'thejeffbyrnes@gmail.com'
 license          'Apache-2.0'
 description      'Installs/Configures zookeeper'
 version          '14.0.0'
+chef_version     '>= 15.3'
 
 issues_url 'https://github.com/evertrue/zookeeper-cookbook/issues'
 source_url 'https://github.com/evertrue/zookeeper-cookbook/'
 
-supports         'ubuntu', '>= 16.04'
+supports         'ubuntu', '>= 18.04'
 supports         'centos', '>= 7.0'
-supports         'oracle', '>= 7.0'
 supports         'redhat', '>= 7.0'
 
-chef_version     '>= 15.0'
 depends          'java', '~> 8.2'
 depends          'magic', '>= 1.1'
 depends          'ark', '~> 5.0'
